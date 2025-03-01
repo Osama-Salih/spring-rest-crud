f53f8a712352beae13365478acdcb9e1634420b4
Osama Salih <osamasalih.ca@gmail.com>
2025-03-01 23:24:27 +0300

    Student (entity, findAll, findOneById)

--- src/main/java/com/luv2code/demo/rest/demoRestController.java
+++ src/main/java/com/luv2code/demo/rest/demoRestController.java
@@ -1,15 +1,37 @@
 package com.luv2code.demo.rest;
 
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api")
 public class demoRestController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-       return "Hello World!";
+    List<Student> students;
+
+    @PostConstruct
+    private void loadData() {
+        students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario","Rossi"));
+        students.add(new Student("Mary", "Smith"));
     }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+   @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
+   }
 }
